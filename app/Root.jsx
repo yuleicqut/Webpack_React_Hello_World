8160d41302dca8e01e04b4150e2804c451fc31b9
yuleicqut <yuleicqit@qq.com>
2016-11-01 21:11:00 +0800

    - Project name: - version: - modification: - by: Andrew Yu

--- app/Root.jsx
+++ app/Root.jsx
@@ -1,18 +1,25 @@
 // Root.jsx
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-// import Hello from './main.jsx'
+import Hello from './main.jsx'
 
-class Hello extends component{
-	construtor()
-	{
-		// super(this.props);
-	}
-	render(){
-		return (<div>Hello, {this.props.name}</div>);
-	}
+// class Hello extends component{
+// 	construtor()
+// 	{
+// 		// super(this.props);
+// 	}
+// 	render(){
+// 		return (<div>Hello, {this.props.name}</div>);
+// 	}
 
-}
+// }
+
+// var Hello = React.createClass(
+// {
+// 	render: function(){
+// 		return <h3>hello, {this.props.name}!</h3>;
+// 	}
+// })
 
 
 render(<Hello name="world"/>, document.getElementById('root'));
--- app/main.jsx
+++ app/main.jsx
@@ -4,12 +4,24 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 
-class Hello extends component{
+class Hello extends Component{
+	construtor(){
+		// super(this.props);
+	}
 	render(){
-		return (<div>Hello, {this.props.name}</div>);
+		return (<h3>Hello, {this.props.name}</h3>);
 	}
 
 }
+
+
+// var Hello = React.createClass(
+// {
+// 	render: function(){
+// 		return <h3>hello, {this.props.name}!</h3>;
+// 	}
+// })
+
 // render(<Hello name="world"/>, document.getElementById('root'));
 export default Hello;
  // render(<div> hello, world</div>, document.getElementById('root'));
